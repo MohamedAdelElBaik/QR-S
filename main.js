97760f033dc35e20d353fdc513651c84de0bd098
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-03-19 03:14:04 +0200

    reorder the code

--- main.js
+++ main.js
@@ -2,45 +2,83 @@ const api = "https://kind-puce-python-shoe.cyclic.app/";
 
 const emailInput = document.querySelector(".email");
 const passwordInput = document.querySelector(".password");
-const btnLogin = document.querySelector(".btnLogin");
-const errorDisplay = document.querySelector(".error");
+
 const container_1 = document.querySelector(".container_1");
 const container_2 = document.querySelector(".container_2");
 const container_3 = document.querySelector(".container_3");
+const failMessage = document.querySelector(".failMessage");
+const statusHeader = document.querySelector(".statusHeader");
+
 const studentName = document.querySelector(".student--name");
 const studentLevel = document.querySelector(".student--level");
 const studentId = document.querySelector(".student--id");
+
+const errorDisplay = document.querySelector(".error");
+const requestStatus = document.querySelector(".requestStatus");
+const successMessage = document.querySelector(".successMessage");
+
+const btnLogin = document.querySelector(".btnLogin");
 const btnScan = document.querySelector(".btnScan");
 const btnScanAgain = document.querySelector(".btnScanAgain");
-const requestStatus = document.querySelector(".requestStatus");
 const btnBackToMain = document.querySelectorAll(".btnBackToMain");
-const successMessage = document.querySelector(".successMessage");
-const failMessage = document.querySelector(".failMessage");
-const statusHeader = document.querySelector(".statusHeader");
 
 let token = "";
 let studentData = "";
 let lectureId = "";
 
+// functions
 const addStudentData = (data) => {
   studentName.textContent = data.name;
   studentLevel.textContent = data.level;
   studentId.textContent = data.sittingNumber;
 };
 
-btnLogin.addEventListener("click", () => {
-  studentData = "";
-  errorDisplay.textContent = "";
+const loginResult = () => {
+  if (studentData) {
+    addStudentData(studentData);
+    container_1.classList.add("hidden");
+    container_2.classList.remove("hidden");
+  } else {
+    errorDisplay.textContent = "البيانات التي ادخلتها ليست صحيحة";
+  }
+};
 
-  const emailValue = emailInput.value;
-  const passwordValue = passwordInput.value;
+const qrScan = () => {
+  var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
+    fps: 10,
+    qrbox: 350,
+    aspectRatio: 1.777778,
+  });
 
-  console.log(emailValue);
-  console.log(passwordValue);
+  function onScanSuccess(decodedText, decodedResult) {
+    lectureId = decodedText;
+    console.log(`Scan result: ${decodedText}`, decodedResult);
 
-  if (!emailValue || !passwordValue)
-    return (errorDisplay.textContent = "اسم المستخدم وكلمة السر مطلوبة");
+    sendAttendanceRequest(decodedText);
+    html5QrcodeScanner.clear(decodedText);
+  }
 
+  html5QrcodeScanner.render(onScanSuccess);
+};
+
+const displayResult = (response) => {
+  if (response.status === "success") {
+    successMessage.classList.remove("hidden");
+  } else if (response.status === "fail") {
+    failMessage.classList.remove("hidden");
+    if (response.message === "You are already attended") {
+      requestStatus.textContent = "انت بالفعل مسجل في هذه المحاضرة";
+      // requestStatus.textContent = ;
+    } else {
+      btnScanAgain.classList.remove("hidden");
+      statusHeader.textContent = "حدثت مشكلة !!";
+      requestStatus.textContent = "حاول مره اخري";
+    }
+  }
+};
+
+// requests
+const loginRequest = (emailValue, passwordValue) => {
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -59,31 +97,9 @@ btnLogin.addEventListener("click", () => {
     })
     .then((__) => console.log(studentData, token))
     .then((__) => {
-      if (studentData) {
-        addStudentData(studentData);
-        container_1.classList.add("hidden");
-        container_2.classList.remove("hidden");
-      } else {
-        errorDisplay.textContent = "البيانات التي ادخلتها ليست صحيحة";
-      }
+      loginResult();
     })
     .catch((error) => console.error(error));
-});
-
-const displayResult = (response) => {
-  if (response.status === "success") {
-    successMessage.classList.remove("hidden");
-  } else if (response.status === "fail") {
-    failMessage.classList.remove("hidden");
-    if (response.message === "You are already attended") {
-      requestStatus.textContent = "انت بالفعل مسجل في هذه المحاضرة";
-      // requestStatus.textContent = ;
-    } else {
-      btnScanAgain.classList.remove("hidden");
-      statusHeader.textContent = "حدثت مشكلة !!";
-      requestStatus.textContent = "حاول مره اخري";
-    }
-  }
 };
 
 const sendAttendanceRequest = (lecId) => {
@@ -112,25 +128,29 @@ const sendAttendanceRequest = (lecId) => {
     });
 };
 
+// events
+btnLogin.addEventListener("click", () => {
+  studentData = "";
+  errorDisplay.textContent = "";
+
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  console.log(emailValue);
+  console.log(passwordValue);
+
+  if (!emailValue || !passwordValue)
+    return (errorDisplay.textContent = "اسم المستخدم وكلمة السر مطلوبة");
+
+  loginRequest(emailValue, passwordValue);
+});
+
 btnScan.addEventListener("click", () => {
   container_2.classList.add("hidden");
   container_3.classList.remove("hidden");
   console.log("clicked");
-  var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
-    fps: 10,
-    qrbox: 350,
-    aspectRatio: 1.777778,
-  });
 
-  function onScanSuccess(decodedText, decodedResult) {
-    lectureId = decodedText;
-    console.log(`Scan result: ${decodedText}`, decodedResult);
-
-    sendAttendanceRequest(decodedText);
-    html5QrcodeScanner.clear(decodedText);
-  }
-
-  html5QrcodeScanner.render(onScanSuccess);
+  qrScan();
 });
 
 btnBackToMain.forEach((btn) => {
